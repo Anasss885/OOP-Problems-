c9a13296d6f16fc0463a152ac0cd3d7cb4b48c22
Anasss885 <62183487+Anasss885@users.noreply.github.com>
2023-03-24 17:13:51 +0200

    Update and rename Problem 1 (create a simple program using oop) to Problem 1 (create a simple program using oop).cpp

--- Problem 1 (create a simple program using oop).cpp
+++ Problem 1 (create a simple program using oop).cpp
@@ -1,29 +1,11 @@
-Q1 : create a class that accept id,age,name from a user and print them (YOU MUST USE OOP NOT A PROCEDURAL CODE)
+//Q1 : create a class that accept id,age,name from a user and print them (YOU MUST USE OOP NOT A PROCEDURAL CODE)
 
 
 
-Solution :
+//Solution :
 
 
 #include<iostream>
-#include <algorithm> 
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <deque>
-#include<string>
-#include <set> 
-#include <map> 
-#include <cstdlib>
-#include <utility>
-#include <ctime>
-#include <cmath>
-#include <valarray>
-#include<sstream>  
-#include <stdio.h>
-#include <string.h>
 #include <iomanip> 
 using namespace std;
 #define ANOOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
