6989aecfa21b782464b7cb07319a855b26c41ca2
Anasss885 <62183487+Anasss885@users.noreply.github.com>
2023-03-24 17:14:17 +0200

    Update and rename Problem 3 to Problem 3.cpp

--- Problem 3.cpp
+++ Problem 3.cpp
@@ -1,30 +1,12 @@
-Q:
+/*Q:
 Create App that Accepts 3 numbers and return their sum (using oop conecpts )
-NOTE: Make sure that 3 numbers are evens.
+NOTE: Make sure that 3 numbers are evens.*/
 
 
-Solution :
+//Solution :
 
 #include<iostream>
-#include <algorithm> 
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <deque>
-#include<string>
-#include <set> 
-#include <map> 
-#include <cstdlib>
-#include <utility>
-#include <ctime>
-#include <cmath>
-#include <valarray>
-#include<sstream>  
-#include <stdio.h>
-#include <string.h>
-#include <iomanip> 
+
 using namespace std;
 #define ANOOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define ll long long
