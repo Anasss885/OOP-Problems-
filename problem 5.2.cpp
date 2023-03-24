3be924e6467e7b147f781dbc1fa633ddefaee4ca
Anasss885 <62183487+Anasss885@users.noreply.github.com>
2023-03-24 17:17:15 +0200

    Update and rename problem 5.2 to problem 5.2.cpp

--- problem 5.2.cpp
+++ problem 5.2.cpp
@@ -1,31 +1,12 @@
-Q:
+/*Q:
 Create tha app that accept 
 Person(id,name)
 Player(id,name,number,phone)
 studnet(id,name,age,phone)
 
-Solution:
+Solution:             */
 
 #include<iostream>
-#include <algorithm> 
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <deque>
-#include<string>
-#include <set> 
-#include <map> 
-#include <cstdlib>
-#include <utility>
-#include <ctime>
-#include <cmath>
-#include <valarray>
-#include<sstream>  
-#include <stdio.h>
-#include <string.h>
-#include <iomanip> 
 using namespace std;
 #define ANOOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define ll long long
